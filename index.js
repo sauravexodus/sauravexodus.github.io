5460c3dd2b2a1993696d4dee7017c39e60e68c6b
Sourav Chandra <saurav.chandra1992@live.com>
2021-07-19 05:56:31 +0530

    Add animation of closing and opening

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-document.querySelector('.stats-container').addEventListener('click', function(event) {
-  const eventTarget = event.target;
-  eventTarget.classList.replace("collapsed", "expanded")
+const statsContainer = document.querySelector('.stats-container')
+statsContainer.addEventListener('click', function(event) {
+  if (statsContainer.classList.contains("collapsed")) {
+    statsContainer.classList.replace("collapsed", "expanded")
+  }
+})
+
+const close = document.querySelector('.stats-container .close')
+close.addEventListener('click', function(event) {
+  statsContainer.classList.replace("expanded", "collapsed")
+  event.stopPropagation();
 })
